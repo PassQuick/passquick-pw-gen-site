c4f45ab75c410d38f8063891734652a39efc214f
livingsamurai <33865802+Pavel8694@users.noreply.github.com>
2025-07-16 22:11:28 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = 'terminal-pwgen-v1';
+const filesToCache = [
+  '/',
+  '/main.html',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
