de349024786b93b6d2453dcc0e5382ed4f9ae4c1
livingsamurai <33865802+Pavel8694@users.noreply.github.com>
2025-07-25 03:34:07 +0900

    Add files via upload

--- script_landing.js
+++ script_landing.js
@@ -113,7 +113,7 @@ function goToRussian() {
     document.body.style.opacity = '0.8';
     setTimeout(() => {
         // Replace with your Russian version URL
-        window.location.href = 'pw_generator_ru?v=2';
+        window.location.href = 'pw_generator_ru?v=4';
     }, 300);
 }
 
@@ -122,7 +122,7 @@ function goToEnglish() {
     document.body.style.opacity = '0.8';
     setTimeout(() => {
         // Replace with your English version URL
-        window.location.href = 'pw_generator_en?v=2';
+        window.location.href = 'pw_generator_en?v=4';
     }, 300);
 }
 
